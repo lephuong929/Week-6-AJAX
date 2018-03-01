a48b7ff185a4cf9fdf64ab1a8ddebf76b6285ffa
Phuong <le.phuong.929@gmail.com>
2018-02-28 22:42:13 -0500

    updated 2/28

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,6 +1,6 @@
 //Initial array of basketball players
 
-var basketballPlayers = ["Lebron James", "Kevin Durant", "Kobe Bryant", "Stephen Curry", "Draymond Green", "Russell Westbrook", "James Harden", "Chris Paul", "Michael Jordan", "Kyrie Irving"];
+var basketballPlayers = ["Lebron James", "Kevin Durant", "Kobe Bryant", "Stephen Curry", "Draymond Green", "Russell Westbrook", "James Harden", "Chris Paul", "Michael Jordan", "Kyrie Irving", "Rasheed Wallace", "Vince Carter", "Tracy McGrady", "Tim Duncan", "Shaquille O'Neal"];
 
 // Function for creating basketball player buttons
 renderButtons = function() {
@@ -27,7 +27,8 @@ renderButtons = function() {
 }
 
 // Event listener for all button elements
-$(".basketball-button").on("click", function() {
+$(document).on("click", ".basketball-button", function() {
+	
 	// In this case, the "this" keyword refers to the button that was clicked
 	var player = $(this).attr("data-person");
 
@@ -41,7 +42,10 @@ $(".basketball-button").on("click", function() {
 	method: "GET"
 	})
 	// After the data comes back from the API
-	.then(function(response) {
+	.then(function(response) { 				
+		//Emptying the div
+		$("#gifs-appear-here").empty();
+		
 		// Storing an array of results in the results variable
 		var results = response.data;
 		console.log(response);
@@ -51,8 +55,8 @@ $(".basketball-button").on("click", function() {
 
 			// Only taking action if the photo has an appropriate rating
 			if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-				// Creating a div with the class "item"
-				var gifDiv = $("<div class='item'>");
+				// Creating a div
+				var gifDiv = $("<div>");
 
 				// Storing the result item's rating
 				var rating = results[i].rating;
@@ -64,17 +68,63 @@ $(".basketball-button").on("click", function() {
 	        	var playerImage = $("<img>");
 
 	        	// Giving the image tag an src attribute of a proprty pulled off the result item
-	        	playerImage.attr("src", results[i].images.fixed_height.url);
+	        	playerImage.attr("src", results[i].images.fixed_height_still.url);
+	        	playerImage.attr("alt", "player-image");
+	        	playerImage.attr("class", "gif");
+	        	playerImage.attr("data-state", "still");
+	        	playerImage.attr("data-still", results[i].images.fixed_height_still.url);
+	        	playerImage.attr("data-animate", results[i].images.fixed_height.url);
 
 				// Appending the paragraph to the "gifDiv" div
+				
 				gifDiv.append(p);
 				gifDiv.append(playerImage);
 
 				// Prepending the gifDiv to the "#gifLocation" div in the HTML
+
 				$("#gifs-appear-here").prepend(gifDiv);
 			}
 		}
 	});
 });
+
+$(document).on("click", ".gif", function() {
+  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+  var state = $(this).attr("data-state");
+  var still = $(this).attr("data-still");
+  var animate = $(this).attr("data-animate");
+
+	if (state === "still") {
+	$(this).attr("src", $(this).attr("data-animate"));
+	$(this).attr("data-state", "animate");
+	} else {
+	$(this).attr("src", $(this).attr("data-still"));
+	$(this).attr("data-state", "still");
+	}
+});
+
+
+// This function handles events where a basketball button is clicked
+$("#add-player").on("click", function(event) {
+	event.preventDefault();
+	// This line grabs the input from the textbox
+	var player = $("#player-input").val().trim();
+
+	//Creating an if statement for when the input field is blank but user submits
+	if (player === "") {
+          return false;
+        }
+        else {
+
+	// Adding movie from the textbox to our array
+	basketballPlayers.push(player);
+	$("#player-input").val("");
+
+	// Calling renderButtons which handles the processing of our movie array
+	renderButtons();
+	}
+});
+
+
 
 renderButtons();
